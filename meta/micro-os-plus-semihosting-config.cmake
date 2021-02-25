721e79f739325d4082138db3f5c852c129ee03a5
Liviu Ionescu <ilg@livius.net>
2021-02-25 16:45:56 +0200

    cmake: simplify, keep only config.cmake

--- meta/micro-os-plus-semihosting-config.cmake
+++ meta/micro-os-plus-semihosting-config.cmake
@@ -12,11 +12,61 @@
 # https://cmake.org/cmake/help/v3.18/
 # https://cmake.org/cmake/help/v3.18/manual/cmake-packages.7.html#package-configuration-file
 
-# message(STATUS "Including micro-os-plus-semihosting-config...")
+if(micro-os-plus-semihosting-included)
+  return()
+endif()
+
+set(micro-os-plus-semihosting-included TRUE)
+
+message(STATUS "Including micro-os-plus-semihosting...")
+
+# -----------------------------------------------------------------------------
+# Dependencies.
+
+find_package(micro-os-plus-diag-trace REQUIRED)
+
+# -----------------------------------------------------------------------------
+# The current folder.
+
+get_filename_component(xpack_current_folder ${CMAKE_CURRENT_LIST_DIR} DIRECTORY)
 
 # -----------------------------------------------------------------------------
 
-include("${CMAKE_CURRENT_LIST_DIR}/xpack-helper.cmake")
+if(NOT TARGET micro-os-plus-semihosting-static)
+
+  add_library(micro-os-plus-semihosting-static STATIC EXCLUDE_FROM_ALL)
+
+  # ---------------------------------------------------------------------------
+
+  target_sources(
+    micro-os-plus-semihosting-static
+
+    PRIVATE
+      ${xpack_current_folder}/src/syscalls-semihosting.cpp
+      ${xpack_current_folder}/src/trace-semihosting.cpp
+  )
+
+  target_include_directories(
+    micro-os-plus-semihosting-static
+
+    PUBLIC
+      ${xpack_current_folder}/include
+  )
+
+  target_link_libraries(
+    micro-os-plus-semihosting-static
+    
+    PUBLIC
+      micro-os-plus::diag-trace-static
+      micro-os-plus::architecture
+  )
+
+  # ---------------------------------------------------------------------------
+  # Aliases.
+
+  add_library(micro-os-plus::semihosting-static ALIAS micro-os-plus-semihosting-static)
+  message(STATUS "micro-os-plus::semihosting-static")
+
+endif()
 
 # -----------------------------------------------------------------------------
-
