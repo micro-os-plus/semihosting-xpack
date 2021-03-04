e8ef21718a8b2ba4ec32d3f6762fe21f892663de
Liviu Ionescu <ilg@livius.net>
2021-03-04 10:20:31 +0200

    cmake: use xpack_glob_recurse_cxx()

--- meta/micro-os-plus-semihosting-config.cmake
+++ meta/micro-os-plus-semihosting-config.cmake
@@ -38,7 +38,8 @@ if(NOT TARGET micro-os-plus-semihosting-static)
 
   # ---------------------------------------------------------------------------
 
-  file(GLOB_RECURSE source_files CONFIGURE_DEPENDS "${xpack_current_folder}/src/*.c*")
+  xpack_glob_recurse_cxx(source_files "${xpack_current_folder}/src")
+  xpack_display_relative_paths("${source_files}" "${xpack_current_folder}")
 
   target_sources(
     micro-os-plus-semihosting-static
